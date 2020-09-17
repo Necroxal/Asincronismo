ad10a50361678ef8b10e1105c10a99f228b4b7ba
German <gnrg_74@hotmail.com>
2020-09-17 14:02:22 -0500

    se comento codigo

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -1,17 +1,26 @@
-const fetchdata =  require('../utils/fetchdata');
+// importamos la funcion
+const fetchData = require('./../utils/fetchData');
+// declaramos la ruta de la api
 const API = 'https://rickandmortyapi.com/api/character/';
 
-fetchdata(API)
-.then(data =>{
+fetchData(API)
+  .then(data => {
+    // imprimimos el numero de personajes
     console.log(data.info.count);
-    return fetchdata(`${API}${data.results[0].id}`)
-})
-.then(data =>{
-    console.log(data.name)
-    return fetchdata(data.origin.url);
-    
-})
-.then(data =>{
+    // volvemos a hacer la promesa de pedir algo, en este caso el personaje 1: Rick
+    return fetchData(`${API}${data.results[0].id}`);
+  })
+  .then(data => {
+    // esperamos la promesa anterior y vemos el nombre de rick
+    console.log(data.name);
+    // volvemos a hacer la promesa, pero esta es sobre la dimension de Rick
+    return fetchData(data.origin.url)
+  })
+  .then(data => {
+    // vemos la dimension de rick
     console.log(data.dimension);
-})
-.catch(err => console.error(err));
+  })
+  // si hay error
+  .catch(err => {
+    console.log(err);
+  })
