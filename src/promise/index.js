4db65ebd2a79eee0006dc08410ee3cdc9cc708bd
German <gnrg_74@hotmail.com>
2020-09-17 13:18:47 -0500

    Se garago funcion para multiples promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -9,5 +9,31 @@ const somethingWillHappen = () => {
 };
 
 somethingWillHappen()
-.then(response => console.log(response))
-.catch(err => console.error(err))
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error('No se pudo de neuvo ese');  // Error que nos permite ver en consola más detalles del error
+            reject(error);
+        }
+    });
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+//Correr varias promesas en encadenadas
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
