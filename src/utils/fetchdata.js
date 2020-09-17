9cced773413c8efe4015a33e96ab67571a7d8d18
German <gnrg_74@hotmail.com>
2020-09-17 13:54:59 -0500

    se creo nueva caroeta para trabajar con promesas con al api de rockandmorty

---
+++ src/utils/fetchdata.js
@@ -0,0 +1,21 @@
+// importamos el modulo xmlhttprequest
+const XMLHTTPRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
+const fetchData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHTTPRequest();
+        xhttp.open('GET', url_api, true);
+        xhttp.onreadystatechange = (()=> {
+            if (xhttp.readyState === 4) {
+                (xhttp.status === 200) ?
+                resolve(JSON.parse(xhttp.responseText)): reject(new Error('Error', url_api))
+
+            }
+        });
+        // enviamos la solicitud
+        xhttp.send();
+    });
+}
+
+module.exports = fetchData;
