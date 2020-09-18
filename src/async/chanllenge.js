58e06cc545b0fca9638969665443797fadede61e
German <gnrg_74@hotmail.com>
2020-09-18 18:44:30 -0500

    cambios

--- src/async/chanllenge.js
+++ src/async/chanllenge.js
@@ -1,2 +1,20 @@
-const fecthData = require('../utils/fetchdata');
+const fetchData = require('../utils/fetchData');
 const API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFuncton = async (url_api) =>{
+    try{
+        const data = await fetchData(url_api);
+        const character = await fetchData(`${url_api}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+        
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+    }catch(e){
+        console.log(e);
+    }
+};
+
+console.log('before');
+anotherFuncton(API);
+console.log('after')
